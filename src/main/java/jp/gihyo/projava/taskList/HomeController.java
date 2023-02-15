009bca025ab635b7fa2827eebe161239d1b9b2f2
onoderaryo <ryo.program.school@gmail.com>
2023-02-15 23:47:16 +0900

    tasklist_home&hello_html

--- src/main/java/jp/gihyo/projava/taskList/HomeController.java
+++ src/main/java/jp/gihyo/projava/taskList/HomeController.java
@@ -1,6 +1,7 @@
 package jp.gihyo.projava.taskList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
@@ -8,17 +9,8 @@ import java.time.LocalDateTime;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                        <h1>Hello</h1>
-                        It woorks!<br>
-                        現在時刻は%Sです。
-                    </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 }
